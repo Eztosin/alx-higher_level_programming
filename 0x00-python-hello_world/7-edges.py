c1d75dc178ea34aa003bef2ae8e452f56319eaaa
root <Eztosin>
2023-08-08 20:08:02 +0000

    Copy - Cut - Paste

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 word_first_3 = word[0:3]
 print(f"First 3 letters: {word_first_3}")
 word_last_2 = word[-2:]
-print(f"last 2 letters: {word_last_2}")
+print(f"Last 2 letters: {word_last_2}")
 middle_word = word[1:-1]
 print(f"Middle word: {middle_word}")
