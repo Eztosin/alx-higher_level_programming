b4b2f8be0ba815c722844698e16935eb2735e96e
root <Eztosin>
2023-08-16 01:01:10 +0000

    Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,6 +9,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) >= 2 or len(tuple_b) >= 1:
         return (result_1, result_2)
     elif len(tuple_a) == 0 and len(tuple_b) == 0:
-        return 0
-    else:
         return None
