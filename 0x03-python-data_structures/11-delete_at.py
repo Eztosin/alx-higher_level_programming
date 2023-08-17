816b1e65b582359f9f88588a7fe60575fd7a70f9
root <Eztosin>
2023-08-17 11:05:58 +0000

    Delete at

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    empty_index = my_list.copy()
-    if idx >= len(my_list) or idx < 0:
-        return empty_index
-    empty_index = my_list[:idx] + my_list[idx+1:]
-    return empty_index
+    if idx < len(my_list) or idx >= 0:
+        del (my_list[idx])
+    return my_list
