d513633d55f13c55d6606340c488a7b9444f9b67
root <Eztosin>
2023-08-17 09:16:52 +0000

    Update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for i in sorted(a_dictionary.keys()):
-        if key:
-            a_dictionary[i] = value
-        else:
-            a_dictionary[i] = key
+    a_dictionary[key] = value
     return a_dictionary
