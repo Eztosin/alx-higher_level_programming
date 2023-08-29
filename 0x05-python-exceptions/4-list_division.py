3ee4713ebb5514a762fdf7b67037df43989e3822
root <Eztosin>
2023-08-29 03:16:03 +0000

    Divide a list

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            numerator = my_list_1[i]
+            denominator = my_list_2[i]
+            if not (isinstance(numerator, (int, float)) and
+                    isinstance(denominator, (int, float))):
+                print("wrong type")
+                result.append(0)
+            elif denominator == 0:
+                print("division by 0")
+                result.append(0)
+            else:
+                result.append(numerator / denominator)
+        except IndexError:
+            print("out of range")
+            result.append(0)
+    return result
