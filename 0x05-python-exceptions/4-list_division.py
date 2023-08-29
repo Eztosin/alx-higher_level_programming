1c581703a2fc19e569cba52b1f59a7c02d7f59f2
root <Eztosin>
2023-08-29 14:19:25 +0000

    Update Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,6 +5,10 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             numerator = my_list_1[i]
             denominator = my_list_2[i]
+        except IndexError:
+            print("out of range")
+            result.append(0)
+        finally:
             if not (isinstance(numerator, (int, float)) and
                     isinstance(denominator, (int, float))):
                 print("wrong type")
@@ -14,7 +18,4 @@ def list_division(my_list_1, my_list_2, list_length):
                 result.append(0)
             else:
                 result.append(numerator / denominator)
-        except IndexError:
-            print("out of range")
-            result.append(0)
     return result
