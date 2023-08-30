b37b0564d3c33d33dba19dc6a558c9a838c41975
root <Eztosin>
2023-08-30 21:20:36 +0000

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Defines a class square based on 0-square.py"""
+"""Defines a class square"""
 
 
 class Square:
+    """creates a new instance of the Square
+    class and assigns it to the variable size"""
     def __init__(self, size):
         self.__size = size
