6b90bd82ab1927b11f5838d8e9fdcc1d8aace752
root <Eztosin>
2023-08-31 03:23:37 +0000

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a class square"""
+
+
+class Square:
+    """creates an instance of the square class
+    and assigns it to the variable size"""
+
+    def __init__(self, size=0):
+
+        """Checking if size is a valid integer and or less than
+        zero"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be an >= 0")
+        self.__size = size
