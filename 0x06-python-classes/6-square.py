f4c47cbb73e1dfacab86eb814fbcd8b730e4d4c9
root <Eztosin>
2023-09-02 14:59:35 +0000

    Completed 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -58,5 +58,7 @@ class Square:
             for _ in range(self.__position[1] > 0):
                 print()
             for _ in range(self.__size):
-                print("{}".format(" " * self.__position[0] + "#" *
-                                  self.__size))
+                if self.__position[0] > 0:
+                    print("{}".format(" " * self.__position[0]),
+                          end='')
+                print("{}".format("#" * self.__size))
