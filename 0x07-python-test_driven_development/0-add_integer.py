cc598aea322c715e19e037b9d82f08aee1ae60ab
root <Eztosin>
2023-09-03 21:30:34 +0000

    Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """ Function that adds 2 integers
+    Args:
+        a (int or float): First argument
+        b (int or float, optional parameter): Second argument
+
+    Returns:
+         int: The sum of parameter a and b
+    """
+
+    if (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+        x = int(a)
+        y = int(b)
+        return (x + y)
+    elif not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
