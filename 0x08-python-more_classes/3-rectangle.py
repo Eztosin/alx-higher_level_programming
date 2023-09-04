8b71a56cc326e93acbb29b4078e9381e46cbcc13
root <Eztosin>
2023-09-04 17:17:50 +0000

    String representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""Defining a rectangle"""
+
+
+class Rectangle:
+    """a class that defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """initializing the class object attributes
+        Args:
+            width: the width of the rectangle
+            height: the height of the rectangle
+        Raises:
+             TypeError: If the value of the width or height is not
+                        an integer.
+             ValueError: If the value of the width or height is
+                         less than 0.
+
+        """
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """getter method to retrieve the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter method to set the width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter method to retrieve the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter method to set the height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return (self.__height * self.__width)
+
+    def perimeter(self):
+        """returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return (self.__height + self.__width) * 2
+
+    def __str__(self):
+        """prints the rectangle with the character "#" """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rect_str = ""
+        for _ in range(self.__height):
+            rect_str += "#" * self.__width + "\n"
+        return rect_str[:-1]
