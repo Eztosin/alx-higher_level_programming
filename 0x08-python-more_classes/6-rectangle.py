286fc74da0d435fee30140daf983d9886ff39f75
root <Eztosin>
2023-09-04 22:19:17 +0000

    How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """a class that defines a rectangle"""
     number_of_instances = 0
-    
+
     def __init__(self, width=0, height=0):
         """initializing the class object attributes
         Args:
