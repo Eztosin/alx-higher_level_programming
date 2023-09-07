8f615c8a529e82e6e19e3801498cb0e9f6580870
root <Eztosin>
2023-09-07 17:00:19 +0000

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+"""function that prints names given as parameters"""
+
+
 def say_my_name(first_name, last_name=""):
-    """function that prints names given as parameters"""
+    """function that prints names given as parameters
+    Args:
+        first_name: First parameter to function
+        last_name: Second parameter to function initialized to ""
+
+    Return:
+         the parameters(string of chars) passed to function.
+
+    Raises:
+         TypeError: If first_name or last_name is not a string.
+    """
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
