e723f45e1cd31f5b243e74437c081c7d2b3f27d3
root <tosinezekiel03@gmail.com>
2023-09-12 21:41:44 +0000

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,7 +3,7 @@
 import sys
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file')\
         .save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file')\
@@ -11,9 +11,9 @@ if __name__ = "__main__":
 
     filename = "add_item.json"
 
-    if exists(filename):
+    try:
         my_list = load_from_json_file(filename)
-    else:
+    except FileNotFoundError:
         my_list = []
 
     my_list.extend(sys.argv[1:])
