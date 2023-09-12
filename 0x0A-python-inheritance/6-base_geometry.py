928d54b17649c7244b1b999c87defacbb1fec40c
root <tosinezekiel03@gmail.com>
2023-09-12 01:06:33 +0000

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module contains a class based on 5-base_geometry.py"""
+
+
+class BaseGeometry:
+    """class contains an empty area method
+    Raises: Exception
+
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
