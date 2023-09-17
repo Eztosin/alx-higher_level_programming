107d791f977677e884467102185d91dc4d902ba0
root <tosinezekiel03@gmail.com>
2023-09-17 16:25:26 +0000

    If it's not tested it doesn't work

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import unittest
+from models.base import Base
+
+
+"""Test cases for base module"""
+
+
+class test_base(unittest.TestCase):
+    """class to test base"""
+    def test_id_valid(self):
+        """test for a valid id"""
+        valid_id = Base(42)
+        self.assertEqual(valid_id.id, 42)
+
+    def test_id_negative(self):
+        """test for a negative id"""
+        negative_num = Base(-10)
+        self.assertEqual(negative_num.id, -10)
+
+    def test_id_string(self):
+        """test for an id that's a string"""
+        char_str = Base("Unittesting")
+        self.assertEqual(char_str.id, "Unittesting")
