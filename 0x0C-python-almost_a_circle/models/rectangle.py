951ce991e827a30f3c06aa7612c97149a7652837
root <tosinezekiel03@gmail.com>
2023-09-17 17:48:33 +0000

    First Rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+"""Rectangle module inherits from Base class"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """a rectangle class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initializing the attributes"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """getter method to get the width of the rectangle"""
+        return self.__width
+
+    @property
+    def height(self):
+        """getter method to get the height of the rectangle"""
+        return self.__height
+
+    @property
+    def x(self):
+        """getter method to get the x coordinate of the rectangle"""
+        return self.__x
+
+    @property
+    def y(self):
+        """getter method to get the y coordinate of the rectangle"""
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        """setter method to set the width of the rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """setter method to set the height of the rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """setter method to set the x coordinate of the rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be > 0")
+
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """setter method to set the y coordinate of the rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be > 0")
+
+        self.__y = value
