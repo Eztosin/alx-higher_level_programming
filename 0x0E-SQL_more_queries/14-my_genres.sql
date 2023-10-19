adf8c31887bb56db79e9c1116a3e076626e4098f
root <tosinezekiel03@gmail.com>
2023-10-19 03:17:11 +0000

    My genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- Import the database dump from hbtn_0d_tvshows to your
+-- MySQL server: download (same as 13-count_shows_by_genre.sql)
+-- a script that uses the hbtn_0d_tvshows database to lists
+-- all genres of the show Dexter
+
+SELECT tv_genres.name FROM tv_genres INNER JOIN tv_shows_genres
+ON tv_genres.id = tv_show_genres.genre_id INNER JOIN tv_shows
+ON tv_shows_genres.show_id=tv_shows.id WHERE
+tv_shows,title="Dexter" ORDER BY tv_genres.name;
