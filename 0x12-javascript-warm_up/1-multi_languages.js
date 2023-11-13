ad8c53f5246494b83189ab5416ebf3f3fcbea016
root <tosinezekiel03@gmail.com>
2023-11-13 22:47:05 +0000

    # languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+a script that prints 3 statement lines
+*/
+let myVar = 'C is fun';
+console.log(myVar);
+
+myVar = 'Python is cool';
+console.log(myVar);
+
+myVar = 'JavaScript is amazing';
+console.log(myVar);
