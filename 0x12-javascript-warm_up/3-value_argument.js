9178ed2a011765768c33e652ff9237fcd986a257
root <tosinezekiel03@gmail.com>
2023-11-13 23:12:25 +0000

    Value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 /*
-a script that prints the first argument passed to it
+ a script that prints the first argument passed to it
 */
-
 const argv = process.argv.slice(2);
 if (argv.length === 0) {
   console.log('No argument');
