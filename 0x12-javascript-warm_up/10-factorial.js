d550849c9fdc0bcff815001eff69b36987d0e3b8
root <tosinezekiel03@gmail.com>
2023-11-14 00:58:40 +0000

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/*
+  a script that computes and prints a factorial
+*/
+const computeFactorial = (n) => {
+  if (isNaN(n) || n < 0) {
+    return (1);
+  }
+
+  if (n === 0) {
+    return (1);
+  }
+
+  return n * computeFactorial(n - 1);
+};
+
+const arg = parseInt(process.argv[2]);
+const result = computeFactorial(arg);
+
+console.log(result);
