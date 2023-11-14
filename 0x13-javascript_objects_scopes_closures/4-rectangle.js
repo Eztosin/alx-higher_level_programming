d079440bf284249d76f2b93e10fa7c3feab897a8
root <tosinezekiel03@gmail.com>
2023-11-14 22:41:19 +0000

    Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+/*
+a class Rectangle that defines a rectangle
+*/
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.width && this.height) {
+      let i = 0;
+      while (i < this.height) {
+        console.log('X'.repeat(this.width));
+        i++;
+      }
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
