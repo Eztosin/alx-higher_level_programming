2b9fdd705d6f39d4890ae3b95153ef67dbb333e5
root <tosinezekiel03@gmail.com>
2023-11-14 00:45:34 +0000

    Add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+  a script that prints the addition of 2 integers
+*/
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+
+console.log(arg1 + arg2);
