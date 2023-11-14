0c1c0a81e418abe8f586533f9b10ccfb8d3536da
root <tosinezekiel03@gmail.com>
2023-11-14 22:08:39 +0000

    Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/*
+ a class Rectangle that defines a rectangle
+*/
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
