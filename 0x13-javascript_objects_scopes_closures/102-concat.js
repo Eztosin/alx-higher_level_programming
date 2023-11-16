fca8993ab449a7aad5e02cc7b89592766274426b
root <tosinezekiel03@gmail.com>
2023-11-16 20:04:30 +0000

    Concat files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+concats 2 files
+*/
+
+const fs = require('fs');
+
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+const textsA = fs.readFileSync(fileA, 'utf8');
+const textsB = fs.readFileSync(fileB, 'utf8');
+const concatenatedTexts = textsA + '\n' + textsB;
+
+fs.writeFileSync(fileC, concatenatedTexts);
