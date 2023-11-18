fdda9eece427a932759f6890b3149b5aedeb0377
root <tosinezekiel03@gmail.com>
2023-11-18 19:14:39 +0000

    Cities by states

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" takes in an argument and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument."""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    """
+    Connects to a MySQL server
+
+    Args:
+    - sys.argv[1]: MySQL username
+    - sys.argv[2]: MySQL password
+    - sys.argv[3]: Name of the database containing the states table
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    name = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database_name)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name=%s "
+                   "ORDER BY id ASC", (name,))
+
+    rows = cursor.fetchall()
+    for r in rows:
+        print(r)
+
+    db.close()
