e9fa426db95143d353d8a67813ca1c5389708c2e
root <tosinezekiel03@gmail.com>
2023-11-18 14:33:55 +0000

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,14 +4,21 @@
 import sys
 import MySQLdb
 
-
 if __name__ == "__main__":
+    """
+    Connects to a MySQL server
+
+    Args:
+    - sys.argv[1]: MySQL username
+    - sys.argv[2]: MySQL password
+    - sys.argv[3]: Name of the database containing the states table
+    """
     username = sys.argv[1]
     password = sys.argv[2]
     database_name = sys.argv[3]
 
 db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                       passwd=password, db=database_name)
+                     passwd=password, db=database_name)
 
 cursor = db.cursor()
 
