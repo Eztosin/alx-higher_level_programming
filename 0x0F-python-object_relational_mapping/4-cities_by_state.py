126b55c4d291deb751c0b8a53409e26e5936076b
root <tosinezekiel03@gmail.com>
2023-11-19 19:43:23 +0000

    Cities by states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" write a script that takes in arguments and displays all
+ values in the states table of hbtn_0e_0_usa where name matches
+ the argument. But this time, write one that is safe from MySQL
+ injections!"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    """
+    Connects to a MySQL server
+
+    Args:
+    - sys.argv[1]: MySQL username
+    - sys.argv[2]: MySQL password
+    - sys.argv[3]: Name of the database containing the states table
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database_name)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM "
+                   "cities INNER JOIN states ON cities.state_id "
+                   "= states.id ORDER BY cities.id ASC")
+
+    rows = cursor.fetchall()
+    for r in rows:
+        print(r)
+
+    db.close()
