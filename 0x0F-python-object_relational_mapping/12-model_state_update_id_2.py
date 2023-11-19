a0b98a0363a504416f661a30864b8e07e213c8cc
root <tosinezekiel03@gmail.com>
2023-11-19 21:26:22 +0000

    Update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     state = session.query(State).filter(State.id == 2).first()
     state.name = "New Mexico"
     session.commit()
