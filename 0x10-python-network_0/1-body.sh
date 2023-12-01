74240afc4a37b218794c7e97d845adc2394754fe
root <tosinezekiel03@gmail.com>
2023-12-01 01:28:19 +0000

    cURL Method

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes a URL, sends a GET request to URL,& displays the body of d resp
+# a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -sL "$1"
