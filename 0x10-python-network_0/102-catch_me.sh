71b4d961e1d437e3f6ee0495f48e5a9547d054a0
root <tosinezekiel03@gmail.com>
2023-12-02 10:33:01 +0000

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a bash script that sends a request to the server to get the response "You got me!"
-curl -s -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool" -L -H "Content-Type: application/json"
+curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
