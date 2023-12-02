fc0d240feeaa900a26be65cfabe4acc84e302a86
root <tosinezekiel03@gmail.com>
2023-12-02 11:23:39 +0000

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
 
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
-    print(f"\t- content: {}".format(response.text))
+    print("\t- content: {}".format(response.text))
