50652da67a1597700320406652a7df78b3942331
root <tosinezekiel03@gmail.com>
2024-01-10 13:13:16 +0000

    Starwars!

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+    if (error) {
+	console.error('Error:', error);
+    } else {
+	if (response.statusCode === 200) {
+	    const movieData = JSON.parse(body);
+	    console.log(`Title of Episode ${movieData.episode_id}: ${movieData.title}`);
+	} else {
+	    console.error(`Failed to retrieve movie data. Status code: ${response.statusCode}`);
+	}
+    }
+});
