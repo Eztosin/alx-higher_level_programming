f95667cb2549559d570b8b6aceeed5fdd4682827
root <tosinezekiel03@gmail.com>
2024-01-10 13:38:49 +0000

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,28 +1,5 @@
 #!/usr/bin/node
 
-// const request = require('request');
-
-// const movieId = process.argv[2];
-
-// if (!movieId || isNaN(movieId)) {
-//   console.error('Please provide a valid movie ID as an argument.');
-//   process.exit(1);
-// }
-
-// const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-// request(apiUrl, (error, response, body) => {
-//     const movieData = JSON.parse(body);
-//     for (let i = 1; i < movieData.results.length + 1; i++) {
-// 	if (i === movieId) {
-// 	    console.log(movieData.results[i - 1].title);
-// 	}
-//     }
-//     if (error) {
-// 	console.error(error);
-//     }
-// });
-
 const request = require('request');
 
 const movieId = process.argv[2];
