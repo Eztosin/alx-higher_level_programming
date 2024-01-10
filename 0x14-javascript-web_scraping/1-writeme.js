e23f8b78b76ede799e2bd441c85a5cf2e2287fb6
root <tosinezekiel03@gmail.com>
2024-01-10 13:01:12 +0000

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,9 +3,9 @@
 const fs = require('fs');
 
 const filePath = process.argv[2];
-const text_file = process.argv[3]
+const textfile = process.argv[3];
 
-fs.writeFile(filePath, text_file, 'utf-8', (err) => {
+fs.writeFile(filePath, textfile, 'utf-8', (err) => {
   if (err) {
     console.error(err);
   }
