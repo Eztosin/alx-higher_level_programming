bdeba6d3bebea0c789e1a6b867dd22f8a41942a7
root <tosinezekiel03@gmail.com>
2024-01-10 14:26:07 +0000

    Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+//a script that gets the contents of a webpage and stores it in a file
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+if (!url || !filePath) {
+  console.error('Please provide both a URL and a file path as arguments.');
+  process.exit(1);
+}
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    if (response.statusCode === 200) {
+      fs.writeFile(filePath, body, 'utf-8', (err) => {
+        if (err) {
+          console.error('Error writing to file:', err);
+        } else {
+          console.log(`The contents from ${url} have been saved to ${filePath}`);
+        }
+      });
+    } else {
+      console.error(`Failed to retrieve content. Status code: ${response.statusCode}`);
+    }
+  }
+});
