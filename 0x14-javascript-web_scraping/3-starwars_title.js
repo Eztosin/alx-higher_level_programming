fa8e0640498c352bbc101282990326a7e001e79c
root <tosinezekiel03@gmail.com>
2024-01-10 13:18:24 +0000

    Starwars!

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,20 +2,23 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
+const movieId = process.argv[2]; // Get the movie ID from the command line argument
 
+if (!movieId || isNaN(movieId)) {
+  console.error('Please provide a valid movie ID as an argument.');
+  process.exit(1);
+}
 
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request(apiUrl, (error, response, body) => {
-    if (error) {
-	console.error('Error:', error);
-    } else {
-	if (response.statusCode === 200) {
-	    const movieData = JSON.parse(body);
-	    console.log(`Title of Episode ${movieData.episode_id}: ${movieData.title}`);
-	} else {
-	    console.error(`Failed to retrieve movie data. Status code: ${response.statusCode}`);
+    const movieData = JSON.parse(body);
+    for (let i = 1; i < movieData.results.length + 1; i++;) {
+	if (i === movieId) {
+	    console.log(movieData.results[i - 1].title);
 	}
+    }
+    if (error) {
+	console.error(error);
     }
 });
