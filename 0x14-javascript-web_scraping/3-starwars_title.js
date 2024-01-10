bdbeb5bf131a17645b528db21eabca07bd77261f
root <tosinezekiel03@gmail.com>
2024-01-10 13:33:55 +0000

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,24 +1,48 @@
 #!/usr/bin/node
 
+// const request = require('request');
+
+// const movieId = process.argv[2];
+
+// if (!movieId || isNaN(movieId)) {
+//   console.error('Please provide a valid movie ID as an argument.');
+//   process.exit(1);
+// }
+
+// const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+// request(apiUrl, (error, response, body) => {
+//     const movieData = JSON.parse(body);
+//     for (let i = 1; i < movieData.results.length + 1; i++) {
+// 	if (i === movieId) {
+// 	    console.log(movieData.results[i - 1].title);
+// 	}
+//     }
+//     if (error) {
+// 	console.error(error);
+//     }
+// });
+
 const request = require('request');
 
-const movieId = parseInt(process.argv[2]);
+const movieId = process.argv[2];
 
 if (!movieId || isNaN(movieId)) {
   console.error('Please provide a valid movie ID as an argument.');
   process.exit(1);
 }
 
-const apiUrl = `https://swapi-api.alx-tools.com/api/films`;
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request(apiUrl, (error, response, body) => {
-    const movieData = JSON.parse(body);
-    for (let i = 1; i < movieData.results.length + 1; i++;) {
-	if (i === movieId) {
-	    console.log(movieData.results[i - 1].title);
-	}
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    if (response.statusCode === 200) {
+      const movieData = JSON.parse(body);
+      console.log(`${movieData.title}`);
+    } else {
+      console.error(`Failed to retrieve movie data. Status code: ${response.statusCode}`);
     }
-    if (error) {
-	console.error(error);
-    }
+  }
 });
